a0fadb64c06eccdb5115b06e8f3d8c7f65a0a835
David Renshaw <dwrenshaw@gmail.com>
2013-02-17 14:12:52 -0500

    record events

--- harness.js
+++ harness.js
@@ -3,27 +3,41 @@ var game = zepto
 var state;
 var canvas;
 
+var ticks;
+var events;
+
+function StampedEvent (stamp, event) {
+    this.stamp = ticks;
+    this.event = event;
+}
+
 function init() {
     canvas = document.getElementById('canvas');
     state = game.init(canvas);
     canvas.onmousedown = mdown;
     window.onkeyup = kup;
     window.onkeydown = kdown;
+    ticks = 0;
+    events = Array();
 }
 
 function tick() {
     state = game.tick(state);
+    ++ticks;
 }
 
 function kup(event) {
+    events.push(new StampedEvent(ticks, event));
     state = game.kup(event, state);
 }
 
 function kdown(event) {
+    events.push(new StampedEvent(ticks, event));
     state = game.kdown(event, state);
 }
 
 function mdown(event) {
+    events.push(new StampedEvent(ticks, event));
     state = game.mdown(event, state);
 }
 
