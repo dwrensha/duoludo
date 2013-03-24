147f67cb7149f1da6649198bebd3970f9baa3ad0
David Renshaw <dwrenshaw@gmail.com>
2013-03-24 14:10:51 -0400

    remove debugging prints

--- client/harness.js
+++ client/harness.js
@@ -223,7 +223,6 @@ var playMode = {
 
 
 function init() {
-    console.log('init');
     stdout = document.getElementById('stdout');
     gameDiv = document.getElementById('game')
 
@@ -235,7 +234,6 @@ function init() {
 
 
 function gotusername() {
-    console.log('got username');
     document.getElementById('username').setAttribute('disabled', 'true');
     document.getElementById('usernamebutton').style.display = 'none';
     document.getElementById('usernamebutton').blur();
