743e7289667e05b208614edb825807d92f648eed
David Renshaw <dwrenshaw@gmail.com>
2013-04-01 14:07:38 -0400

    better cps for leaderboard

--- server/leaderboard.js
+++ server/leaderboard.js
@@ -3,26 +3,16 @@ async = require('async');
 database = require('./database');
 
 // get the best path ending at |checkpoint|
-function getBest(checkpoint, callback) {
-    database.connect (function (db) {
+function getBest(checkpoint, db) {
+    return function (callback) {
         db.collection('paths', function(err, collection) {
-            assert.equal(err, null);
+            if(err) {return callback(err,null);}
 
             // get the one with the smallest value for 'endTicks'
             collection.findOne({endCheckpoint : checkpoint, validFromStart:true},
-                               {'sort': 'endTicks'},
-                               function (err, path) {
-                                   db.close();
-                                   if (err) {
-                                       console.dir(err);
-                                       return;
-                                   }
-                                   console.log('getting best for ' + checkpoint);
-                                   console.log(path.endTicks);
-                                   callback(null, path);
-                               });
+                               {'sort': 'endTicks'}, callback);
         });
-    });
+    };
 }
 
 function getLeaderboard(response) {
@@ -32,12 +22,11 @@ function getLeaderboard(response) {
             assert.equal(err, null);
             collection.distinct('endCheckpoint', {validFromStart:true}, function(err, endCheckpoints) {
                 assert.equal(err, null);
-                db.close();
                 var fns = endCheckpoints.map(
-                    function(x) { return function (cb) {getBest(x, cb)} });
+                    function(cp) { return getBest(cp, db);});
                 async.series(fns, function (err, results) {
+                    db.close();
                     console.log('done. length = ' + results.length);
-
                     response.write(JSON.stringify(results));
                     response.end();
                 });
